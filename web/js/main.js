ae5df377291ed588ec6466a227589b734ffbc397
zhujunhao <1422792458@qq.com>
2020-01-09 14:40:22 +0800

    测试提交

---
+++ web/js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+	$(".heacl").click(function(){
+	    var mr = $(".phonenav").css("right");
+	    if(mr=='-240px'){
+	      $(".phonenav").css("right","0px");
+	    }else{
+	      $(".phonenav").css("right","-240px");
+	    };
+	  });
+	});
+
+
+
+
